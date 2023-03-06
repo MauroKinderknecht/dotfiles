62989a47c8290140ea940401a09b5e485f758033
Mauro <maurokinder@sirius.com.ar>
2023-03-06 20:05:20 -0300

    add

---
+++ base.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source _logger.sh
+
+# Install XCode Command Line Tools
+if ! $(xcode-select --print-path &> /dev/null); then
+  e_pending "Installing XCode Command Line Tools"
+  xcode-select --install &> /dev/null
+
+  until $(xcode-select --print-path &> /dev/null); do
+    sleep 5;
+  done
+fi
+e_success "XCode Command Line Tools installed"
+
+# Install Homebrew
+if ! $(which brew); then
+  e_pending "Installing Homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew doctor
+  brew tap homebrew/cask-fonts
+fi
+e_success "Homebrew installed"
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 source _logger.sh
 
 e_message "Starting setup..."
@@ -15,24 +16,6 @@ if $(which git); then
   e_success "Dotfiles updated"
 fi
 
-# Install XCode Command Line Tools
-if ! $(xcode-select --print-path &> /dev/null); then
-  e_pending "Installing XCode Command Line Tools"
-  xcode-select --install &> /dev/null
-
-  until $(xcode-select --print-path &> /dev/null); do
-    sleep 5;
-  done
-fi
-e_success "XCode Command Line Tools installed"
-
-# Install Homebrew
-if ! $(which brew); then
-  e_pending "Installing Homebrew"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  brew doctor
-  brew tap homebrew/cask-fonts
-fi
-e_success "Homebrew installed"
+source base.sh
 
 e_message "Setup complete!"
